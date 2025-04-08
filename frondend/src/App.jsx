862513ca35a390be6c33c1e6f9fe72525a1dba3a
SHIKHIL8137 <shikhilks870@gmail.com>
2025-04-09 02:20:10 +0530

    react redux installed

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { useState,useEffect } from 'react'
 import { Routes,Route  } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import { Toaster } from 'sonner'
